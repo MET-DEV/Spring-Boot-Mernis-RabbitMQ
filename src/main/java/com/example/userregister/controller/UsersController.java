5a78a7c86a3911bb64de136b58c25c64253f45bd
Mustafa Enes Tepe <72195347+MET-DEV@users.noreply.github.com>
2022-10-01 14:52:15 +0300

    rabbitmq config added

--- src/main/java/com/example/userregister/controller/UsersController.java
+++ src/main/java/com/example/userregister/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.example.userregister.controller;
 import com.example.userregister.mernis.OBKKPSPublicSoap;
 import com.example.userregister.model.User;
 import com.example.userregister.repository.UserRepository;
+import com.example.userregister.service.RabbitMQProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,26 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class UsersController {
     private final UserRepository userRepository;
+    private RabbitMQProducer rabbitMQProducer;
 
     @Autowired
-    public UsersController(UserRepository userRepository) {
+    public UsersController(UserRepository userRepository, RabbitMQProducer rabbitMQProducer) {
         this.userRepository = userRepository;
+        this.rabbitMQProducer = rabbitMQProducer;
     }
 
     @PostMapping("")
     public ResponseEntity<String> register(@RequestBody User user) throws Exception {
         OBKKPSPublicSoap client=new OBKKPSPublicSoap();
         boolean isRealPerson=client.TCKimlikNoDogrula(Long.valueOf(user.getIdentityNumber()),user.getFirstName(),user.getLastName(),user.getBirthOfDateYear());
-        if (isRealPerson){
+        if (!isRealPerson){
             userRepository.save(user);
+            rabbitMQProducer.sendMailAddressToQueue(user.getEmail());
             return ResponseEntity.ok("Kullanıcı başarı ile kayıt edildi");
         }
         return ResponseEntity.badRequest().body("Yanlış kullanıcı bilgileri");
     }
-
-
-
-
-
 
 }
